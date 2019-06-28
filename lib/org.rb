c3e5b738358990c296a099e400624837b934aeff
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-06-28 11:40:29 -0600

    working on longestEvenWord method

--- lib/org.rb
+++ lib/org.rb
@@ -2,8 +2,17 @@ require 'pry'
 
 class Org 
     def longestEvenWord(sentence)
-        # Write your code here
+        words = sentence.split
+        even_words = []
+        words.each do |word|
+            letter_count = word.chars.count
+            if letter_count % 2 == 0 
+                even_words << word
+            end 
+        end
+        
+        even_words.map do |word|
 
+        binding.pry
     end
-
 end 

71d9dde0d914438240b07f65d00c64101669869a
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-07-01 22:46:56 -0600

    worked on functioning Longest Even word Method

--- lib/org.rb
+++ lib/org.rb
@@ -1,46 +1,26 @@
 require 'pry'
 
 class Org 
+    
     def longestEvenWord(sentence)
-        # finds even words
         words = sentence.split
-        even_words = []
-        words.each do |word|
-            letter_count = word.chars.count
-            if letter_count % 2 == 0 
-                even_words << word
-            end 
-        end
+        longest_word= ""
         
-        longest_word = nil
-        # finds longest word
-        even_words.map do |word|
-            if longest_word == nil
-                longest_word = word
-            elsif longest_word.length < word.length
-                longest_word = word 
+        words.each do |word|
+            if word.length % 2 == 0 
+                longest_word = word if word.length 
             end 
         end 
-        longest_word 
-    end
-
+        longest_word
+    end 
+    
     def kDifference(a, k)
         sorted_nums = a.sort.uniq
         sorted_nums.count do |num|
             a.include?(num + k)
         end 
     end
-
-    # def kDifference(a, k)
-    #     count = 0
-    #     sorted_nums = a.sort.uniq
-    #     sorted_nums.map do |num|
-    #         if a.include?(num + k)
-    #             count +=1
-    #         end 
-    #     end 
-    #     count
-    # end 
+    
     
     
     # def countPairs(numbers, k)
@@ -55,9 +35,42 @@ class Org
     #     end
     #     count
     # end 
-
+    
     # def makepairs(numbers)
     #     hash_table = {}
     # end 
 end 
 
+# def kDifference(a, k)
+#     count = 0
+#     sorted_nums = a.sort.uniq
+#     sorted_nums.map do |num|
+#         if a.include?(num + k)
+#             count +=1
+#         end 
+#     end 
+#     count
+# end 
+
+# def longestEvenWord(sentence)
+#     # finds even words
+#     words = sentence.split
+#     even_words = []
+#     words.each do |word|
+#         letter_count = word.chars.count
+#         if letter_count % 2 == 0 
+#             even_words << word
+#         end 
+#     end
+    
+#     longest_word = nil
+#     # finds longest word
+#     even_words.map do |word|
+#         if longest_word == nil
+#             longest_word = word
+#         elsif longest_word.length < word.length
+#             longest_word = word 
+#         end 
+#     end 
+#     longest_word 
+# end
